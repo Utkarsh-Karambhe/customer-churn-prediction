4dd65dcbb582535f33c85c0c557b64b0af6bc2d3
Utkarsh-dot-hi <121947436+Utkarsh-dot-hi@users.noreply.github.com>
2025-06-01 22:15:26 +0530

    Initial commit - Customer Churn Prediction project

---
+++ app.py
@@ -0,0 +1,43 @@
+# Gender -> 1 Female 0 Male
+# Churn -> 1 Yes 0 No
+# Scaler is exported as scaler.pkl
+# Model is exported as model.pkl
+# Order of the X -> 'Age','Gender','Tenure','Monthly Charges'
+
+import streamlit as st
+import joblib 
+import numpy as np
+
+scaler = joblib.load("scaler.pkl")
+model = joblib.load("model.pkl")
+
+st.title("Churn Prediction App")
+
+st.divider()
+
+st.write("Please enter the values and hit the predict button to get the prediction.")
+
+st.divider()
+
+age = st.number_input("Enter Age", min_value=10, max_value=100, value=30)
+gender = st.selectbox("Enter the Gender", ["Male", "Female"])
+tenure = st.number_input("Enter Tenure", min_value = 0, max_value = 130, value=10)
+monthlycharges = st.number_input("Enter Monthly Charge", min_value=30, max_value=150)
+
+st.divider()
+
+st.balloons()
+
+predictbutton = st.button("Predict!")
+
+if predictbutton:
+    gender_selected = 1 if gender == "Female" else 0
+    X=[age, gender_selected,tenure,monthlycharges]
+    X1 = np.array(X)
+    X_array = scaler.transform([X1])
+    prediction = model.predict(X_array)[0]
+    predicted = "Yes" if prediction == 1 else "No"
+    st.write(f"Predicted: {predicted}")
+
+else:
+    st.write("Please enter the values and use predict button")
